6947408c0618220eb9214fd70c7e4fbcfd8585e2
marselich <marsellkaliev7722@gmail.com>
2022-10-04 21:19:39 +0500

    Added coroutines under the hood (state machine)

--- app/src/main/java/ru/kalievmars/coroutinestartlearn/MainActivity.kt
+++ app/src/main/java/ru/kalievmars/coroutinestartlearn/MainActivity.kt
@@ -27,9 +27,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonDownload.setOnClickListener {
-            lifecycleScope.launch {
-                loadData()
-            }
+//            lifecycleScope.launch {
+//                loadData()
+//            }
+            loadDataWithoutCoroutines()
         }
 
     }
@@ -45,9 +46,41 @@ class MainActivity : AppCompatActivity() {
         binding.buttonDownload.isEnabled = true
     }
 
+    // coroutines under the hood is callbacks ( state machine (конечный автомат) )
+    private fun loadDataWithoutCoroutines(step: Int = 0, obj: Any? = null) {
+        when(step) {
+            0 -> {
+                binding.progressBar.isVisible = true
+                binding.buttonDownload.isEnabled = false
+                loadCityWithoutCoroutines {
+                    loadDataWithoutCoroutines(1, it)
+                }
+            }
+            1 -> {
+                val city = obj as String
+                binding.valueCity.text = city
+                loadTemperatureWithoutCoroutines(city) {
+                    loadDataWithoutCoroutines(2, it)
+                }
+            }
+            2 -> {
+                val temp = obj as String
+                binding.valueTemperature.text = temp
+                binding.progressBar.isVisible = false
+                binding.buttonDownload.isEnabled = true
+            }
+        }
+    }
+
     private suspend fun loadCity(): String {
         delay(5000)
         return resources.getString(R.string.tyumen)
+    }
+
+    private fun loadCityWithoutCoroutines(callback: (String) -> Unit) {
+        Handler(Looper.getMainLooper()).postDelayed({
+            callback(resources.getString(R.string.tyumen))
+        }, 5000)
     }
 
     private suspend fun loadTemperature(city: String): String {
@@ -58,6 +91,17 @@ class MainActivity : AppCompatActivity() {
         ).show()
         delay(5000)
         return resources.getString(R.string.value_temperature)
+    }
+
+    private fun loadTemperatureWithoutCoroutines(city: String, callback: (String) -> Unit){
+        Toast.makeText(
+            this,
+            "Search temperature from $city",
+            Toast.LENGTH_LONG
+        ).show()
+        Handler(Looper.getMainLooper()).postDelayed({
+            callback(resources.getString(R.string.value_temperature))
+        }, 5000)
     }
 
 }
