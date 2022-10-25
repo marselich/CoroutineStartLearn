f1bd67e1fcbd7cc4693e0730cb2c0f14d729adf1
marselich <marsellkaliev7722@gmail.com>
2022-10-25 22:49:02 +0500

    Work with job and async await

--- app/src/main/java/ru/kalievmars/coroutinestartlearn/MainActivity.kt
+++ app/src/main/java/ru/kalievmars/coroutinestartlearn/MainActivity.kt
@@ -9,6 +9,7 @@ import android.os.Message
 import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import ru.kalievmars.coroutinestartlearn.databinding.ActivityMainBinding
@@ -30,7 +31,30 @@ class MainActivity : AppCompatActivity() {
 //            lifecycleScope.launch {
 //                loadData()
 //            }
-            loadDataWithoutCoroutines()
+            binding.progressBar.isVisible = true
+            binding.buttonDownload.isEnabled = false
+            val jobCity = lifecycleScope.async {
+                loadCity().apply {
+                    binding.valueCity.text = this
+                }
+            }
+            val jobTemp = lifecycleScope.async {
+                loadTemperature().apply {
+                    binding.valueTemperature.text = this
+                }
+            }
+
+            lifecycleScope.launch {
+                val city = jobCity.await()
+                val temp = jobTemp.await()
+                Toast.makeText(
+                    this@MainActivity,
+                    "$city $temp",
+                    Toast.LENGTH_SHORT
+                ).show()
+                binding.progressBar.isVisible = false
+                binding.buttonDownload.isEnabled = true
+            }
         }
 
     }
@@ -40,7 +64,7 @@ class MainActivity : AppCompatActivity() {
         binding.buttonDownload.isEnabled = false
         val city = loadCity()
         binding.valueCity.text = city
-        val temp = loadTemperature(city)
+        val temp = loadTemperature()
         binding.valueTemperature.text = temp
         binding.progressBar.isVisible = false
         binding.buttonDownload.isEnabled = true
@@ -83,12 +107,7 @@ class MainActivity : AppCompatActivity() {
         }, 5000)
     }
 
-    private suspend fun loadTemperature(city: String): String {
-        Toast.makeText(
-            this,
-            "Search temperature from $city",
-            Toast.LENGTH_LONG
-        ).show()
+    private suspend fun loadTemperature(): String {
         delay(5000)
         return resources.getString(R.string.value_temperature)
     }
